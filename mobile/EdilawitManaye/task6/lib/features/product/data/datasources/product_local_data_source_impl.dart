f4e8ef7d9bf64edc3db7d81e29844321179e49aa
Edilawit Manaye <fortune124a@gmail.com>
2025-08-03 15:10:21 -0700

    Implement local data source and apply testing on it

--- mobile/EdilawitManaye/task6/lib/features/product/data/datasources/product_local_data_source_impl.dart
+++ mobile/EdilawitManaye/task6/lib/features/product/data/datasources/product_local_data_source_impl.dart
@@ -1,15 +1,32 @@
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../../../../core/error/exceptions.dart';
 import '../models/product_model.dart';
 import 'product_local_data_source.dart';
 
-// For this task, the local cache does nothing, so the methods are empty.
+const CACHED_PRODUCTS_KEY = 'CACHED_PRODUCTS';
+
 class ProductLocalDataSourceImpl implements ProductLocalDataSource {
+  // The constructor is now empty.
+
   @override
   Future<void> cacheProducts(List<ProductModel> products) async {
-    return;
+    final prefs = await SharedPreferences.getInstance();
+    final productListJson = products.map((product) => product.toJson()).toList();
+    final jsonString = json.encode(productListJson);
+    await prefs.setString(CACHED_PRODUCTS_KEY, jsonString);
   }
 
   @override
   Future<List<ProductModel>> getLastProducts() async {
-    return [];
+    final prefs = await SharedPreferences.getInstance();
+    final jsonString = prefs.getString(CACHED_PRODUCTS_KEY);
+    if (jsonString != null) {
+      final productList = json.decode(jsonString) as List<dynamic>;
+      final products = productList.map((json) => ProductModel.fromJson(json)).toList();
+      return products;
+    } else {
+      throw CacheException();
+    }
   }
 }
